4f4e7871e013e778b766962fa477c181dcd52e28
홍승주 <hongseungju@hongseungjuui-MacBook-Pro.local>
2021-01-08 01:39:13 +0900

    Complete Mobile Responsive

--- techno.js
+++ techno.js
@@ -1,40 +1,40 @@
+// Welcome Section Slider
 const welcomePrev = document.getElementsByClassName('welcome-prev');
 const welcomeNext = document.getElementsByClassName('welcome-next');
 const welcomeCon = document.getElementsByClassName('welcome-con');
 const welcomePages = document.getElementsByClassName('welcome-page');
 let currentIndex = 1;
-console.log(currentIndex)
-const move = (currentIndex) => {
-  welcomeCon[0].style.transform = 'translateX('+currentIndex*-25+'%)';
-}
 
+// Move Function
+const move = (currentIndex) => welcomeCon[0].style.transform = 'translateX('+currentIndex*-25+'%)'
+
+// Previous Button
 welcomePrev[0].addEventListener('click', () => {
   welcomeCon[0].style.transition = '.5s';
   move(currentIndex - 1);
-  currentIndex-=1;
-  if(currentIndex==0){
+  currentIndex -= 1;
+  if(currentIndex == 0){
     move(0)
-    setTimeout(()=>{
+    setTimeout( () => {
       welcomeCon[0].style.transition = '0s';
       move(2)
-      currentIndex=2;
+      currentIndex = 2;
     }, 501);
   }
-  console.log(currentIndex)
 })
+// Next Button
 welcomeNext[0].addEventListener('click', () => {
   welcomeCon[0].style.transition = '.5s';
-  move(currentIndex + 1);
-  currentIndex+=1;
-  if(currentIndex==welcomePages.length-1){
-    move(welcomePages.length-1)
-    setTimeout(()=>{
+  move( currentIndex + 1);
+  currentIndex += 1;
+  if( currentIndex == welcomePages.length-1 ){
+    move( welcomePages.length-1 )
+    setTimeout( () => {
       welcomeCon[0].style.transition = '0s';
       move(1)
-      currentIndex=1;
+      currentIndex = 1;
     }, 501);
   }
-  console.log(currentIndex)
 })
 move(1)
 
@@ -42,10 +42,22 @@ move(1)
 const pcHeader = document.getElementsByClassName('pc-header');
 
 window.addEventListener('scroll', () => {
-  if(scrollY>300) { 
-    pcHeader[0].classList.add('fill');
-  }
-  else {
-    pcHeader[0].classList.remove('fill');
-  }
+  if( scrollY > 300 ) pcHeader[0].classList.add('fill')
+  else pcHeader[0].classList.remove('fill')
 })
+
+// Header Trigger 
+const headerTrigger = document.getElementsByClassName('header-trigger');
+const nav = document.getElementsByClassName('nav');
+const submenu = document.getElementsByClassName('nav-submenu');
+const navTitle = document.getElementsByClassName('nav-title');
+
+headerTrigger[0].addEventListener('click', ()=> {
+  headerTrigger[0].classList.toggle('active');
+  nav[0].classList.toggle('slide-down');
+})
+for( let i=  0; i < navTitle.length; i++ ) {
+  navTitle[i].addEventListener('click', () => {
+    submenu[i].classList.toggle('active');
+  })
+}
